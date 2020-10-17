cdab089c59af4b14319c41793cf06834b6cdc5fc
x <x>
2020-10-18 01:04:29 +0700

    initial commit

---
+++ src/main/java/com/example/Calculator.java
@@ -0,0 +1,11 @@
+package com.example;
+
+public class Calculator {
+    public float add(float a, float b) {
+        return a + b;
+    }
+
+    public float subtract(float a, float b) {
+        return a - b;
+    }
+}
---
+++ src/test/java/com/example/CalculatorTest.java
@@ -0,0 +1,19 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CalculatorTest {
+    private final Calculator calculator = new Calculator();
+
+    @Test
+    void addTest() {
+        assertEquals(15, calculator.add(10, 5));
+    }
+
+    @Test
+    void subtractTest() {
+        assertEquals(5, calculator.subtract(10, 5));
+    }
+}
